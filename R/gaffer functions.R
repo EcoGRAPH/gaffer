bfff49bd961013dc387f0e88e4cd0a7a46040294
Ramcharan Reddy.K <ramcharankankanala@gmail.com>
2020-07-07 14:47:44 -0500

    Add files via upload

---
+++ R/gaffer functions.R
@@ -0,0 +1,296 @@
+
+#' @title gaffar
+#'
+#' @description This package contains all functions for implementing genetic algorithm for regions clustering.
+#'
+#' @param
+#'
+#' @return
+#'
+#' @examples  evaluategeneration(models=modelsdf,modeldata=mal,adjacency=adjacency),  addgeneration(models=modelsdf,modeldata=mal,covariatenames=envnames,individualspergeneration=individpergeneration,adjacency=adjacency)
+#'
+#' @export library(gaffar)
+
+runint <- function(minint=1, maxint=10, rep=1) {
+
+  runvec <- sample(minint:maxint, rep)
+
+}
+
+rmall <- function() {
+
+  rm(list=ls())
+
+}
+
+# so tired of figuring out why other functions aren't working
+# so I'll just do it the slow, bad way.
+forciblyrevalue <- function(codes=NULL) {
+
+  newcodes <- codes
+
+  if (is.data.frame(codes)) {
+
+    tempcodes <- t(codes)
+    for (curcode in 1:length(unique(sort(tempcodes)))) {
+
+      toreplace <- unique(sort(tempcodes))[curcode]
+      if (!is.na(toreplace)) {
+
+        newcodes[codes == toreplace] <- curcode
+
+      }
+
+    }
+
+  } else {
+
+    for (curcode in 1:length(unique(sort(codes)))) {
+
+      toreplace <- unique(sort(codes))[curcode]
+      if (!is.na(toreplace)) {
+
+        newcodes[codes == toreplace] <- curcode
+
+      }
+    }
+
+  }
+  return(newcodes)
+
+}
+
+evaluategeneration <- function(models=NULL,
+                               modeldata=NULL,
+                               adjacency=NULL) {
+
+  # make sure we start from scratch
+  models$modelmeasure <- Inf
+  models$selectionprobability <- 1
+
+  # create some helpful covariates
+  modeldata$numdate <- as.numeric(modeldata$date)
+  modeldata$doy     <- as.numeric(format(modeldata$date, "%j"))
+
+  # set up the regression
+  # myformula <- formula("objective ~ placeid + s(numdate, by=placeid, id=1) +
+  #                       s(doy, bs='cc', id=2) +
+  #                      cov1 + cov2 + cov3")
+  # myfallbackformula <- formula("objective ~ s(numdate, id=1) +
+  #                       s(doy, bs='cc', id=2) +
+  #                      cov1 + cov2 + cov3")
+
+  myformula <- formula("objective ~ placeid +
+                        s(numdate, by=placeid, id=1) +
+                        cov1 + cov2 + cov3")
+  myfallbackformula <- formula("objective ~ s(numdate, id=1) +
+                       cov1 + cov2 + cov3")
+
+  # run batch_bam on all the models
+  for (curmodelnum in 1:nrow(models)) {
+
+    # put current clusters into model data
+    curclusters <- data.frame(cluster = models$clustermat[curmodelnum,],
+                              placeid = colnames(models$clustermat))
+    curclusters$cluster <- fillbynearest(adjacency=adjacency,
+                                         covariate=models$clustermat[curmodelnum,])
+    modeldata <- left_join(modeldata,
+                           curclusters,
+                           by="placeid")
+
+    # make sure our factors are indeed factors
+    modeldata$cluster <- factor(modeldata$cluster)
+    modeldata$placeid <- factor(modeldata$placeid)
+
+    # figure out which covariates we need
+    modeldata$cov1 <- modeldata[,models$cov1[curmodelnum]]
+    modeldata$cov2 <- modeldata[,models$cov2[curmodelnum]]
+    modeldata$cov3 <- modeldata[,models$cov3[curmodelnum]]
+
+    tryCatch({
+
+      # fit the models
+      modelfit <- batch_bam(data = modeldata,
+                            bamargs = list("formula" = myformula,
+                                           "family" = gaussian(),
+                                           "discrete" = TRUE,
+                                           "nthread" = parallel::detectCores(logical=FALSE)-1),
+                            bamargs_fallback = list("formula" = myfallbackformula),
+                            over = "cluster")
+
+      myAICs <- extractAIC.batch_bam(models=modelfit)
+      models$modelmeasure[curmodelnum] <- sum(myAICs[,2]) + (log(nrow(modeldata)) - 2)*sum(myAICs[,1])
+
+      # models$modelmeasure[curmodelnum] <- sum(extractAIC.batch_bam(models=modelfit)[,2])
+      # models$df <- sum(extract)
+
+      # try cleaning up
+      rm(modelfit)
+      rm(curclusters)
+      modeldata$cluster <- NULL
+      gc()
+
+    }, error = function(e) {
+
+      models$modelmeasure[curmodelnum] <- Inf
+      modeldata$cluster <- NULL
+      rm(list=ls())
+      gc()
+      return(NULL)
+
+    })
+
+  }
+
+  # retain smallest possible frame
+  returnframe <- unlist(models$modelmeasure)
+  rm(list=setdiff(ls(), "returnframe"))
+  gc()
+
+  return(returnframe)
+
+}
+
+fillbynearest <- function(adjacency=NULL,
+                          covariate=NULL) {
+
+  # get list of shortest paths
+  mygraph <- graph.adjacency(adjmatrix=listw2mat(adjacency),
+                             mode="undirected")
+  mypaths <- shortest.paths(graph=mygraph)
+
+  # figure out which of the covariates are NA
+  whichcovsNA <- which(is.na(covariate))
+
+  # set path lengths to infinite to nodes which are NA
+  mypaths[,whichcovsNA] <- Inf
+
+  nearestnonNA <- rep(0, length(covariate))
+  # for each row, figure out which non-NA node is nearest
+  for (i in 1:length(nearestnonNA)) {
+
+    nearestnonNA[i] <- which(mypaths[i,] == min(mypaths[i,]))[1]
+
+  }
+  return(unlist(covariate[nearestnonNA], use.names=FALSE))
+
+}
+
+addgeneration <- function(models=NULL,
+                          modeldata=NULL,
+                          individualspergeneration=10,
+                          covariatenames=NULL,
+                          adjacency=NULL,
+                          mutationprobabilities=c(0.60, 0.30, 0.05, 0.05),
+                          mutationtypes=c("deletenode", "addnode", "recruit", "mutatevariables")) {
+
+  # obtain the last generation
+  lastgennum <- max(models$generation, na.rm=TRUE)
+  lastgen <- models[models$generation == lastgennum,]
+
+  # choose parents from the previous generation according to selectionprobability
+  newgen <- lastgen[sample(x=nrow(lastgen),
+                           size=individualspergeneration,
+                           replace=TRUE,
+                           prob=lastgen$selectionprobability),]
+
+  # for each new child, select a possible mutation
+  newgen$mutation <- sample(x=length(mutationprobabilities),
+                            size=nrow(newgen),
+                            replace=TRUE,
+                            prob=mutationprobabilities)
+  newgen$mutation <- mutationtypes[newgen$mutation]
+
+  # for each new child, mutate as chosen above
+  for (i in 1:(nrow(newgen)-1)) {
+
+    # if this is a merge, then pick two groups to merge
+    if (newgen$mutation[i] == "deletenode") {
+
+      maxgroup <- max(newgen$clustermat[i,], na.rm=TRUE)
+      if (maxgroup >= 2) {
+
+        whichnonNA <- which(!is.na(newgen$clustermat[i,]))
+        whichtodelete <- whichnonNA[sample(x=length(whichnonNA),
+                                           size=1)]
+        newgen$clustermat[i, whichtodelete] <- NA
+
+      } else {
+
+        # if we can't perform a merge, then split instead
+        newgen$mutation[i] <- "addnode"
+
+      }
+
+    }
+
+    # if this is a split, then pick one group to split
+    if (newgen$mutation[i] == "addnode") {
+
+      maxgroup <- max(newgen$clustermat[i,], na.rm=TRUE)
+      whichNA <- which(is.na(newgen$clustermat[i,]))
+      whichtoadd <- whichNA[sample(x=length(whichNA), size=1)]
+      newgen$clustermat[i, whichtoadd] <- (maxgroup+1)
+
+    }
+
+    # if this is a split, then pick one group to split
+    if (newgen$mutation[i] == "recruit") {
+
+      whichNA <- which(is.na(newgen$clustermat[i,]))
+      whichnotNA <- which(!is.na(newgen$clustermat[i,]))
+      whichtoadd <- whichNA[sample(x=length(whichNA), size=1)]
+      whichbeingadded <- whichnotNA[sample(x=length(whichnotNA), size=1)]
+
+      newgen$clustermat[i, whichtoadd] <- newgen$clustermat[i, whichbeingadded]
+
+    }
+
+    # if we're mutating variables
+    if (newgen$mutation[i] == "mutatevariables") {
+
+      # resample the covariates
+      newgen$cov1[i] <- sample(x=covariatenames, size=1)
+      newgen$cov2[i] <- sample(x=covariatenames, size=1)
+      newgen$cov3[i] <- sample(x=covariatenames, size=1)
+
+    }
+
+    # put back in order, make sure we're not missing anything
+    newgen$clustermat[i,] <- forciblyrevalue(newgen$clustermat[i,])
+
+  }
+
+  # retain the alpha
+  previousalpharow <- which(models$modelmeasure == min(models$modelmeasure, na.rm=TRUE))
+  # make sure we're only retaining one
+  previousalpharow <- previousalpharow[1]
+  newgen[nrow(newgen),] <- models[previousalpharow,]
+
+  # # get rid of the mutation signifier
+  # newgen$mutation <- NULL
+
+  # make sure we have model numbers
+  newgen$modelnumber <- 1:nrow(newgen)
+
+  # evaluate new generation
+  newgen$modelmeasure <- evaluategeneration(models=newgen,
+                                            modeldata=mal,
+                                            adjacency=adjacency)
+  # rank models
+  newgen$selectionprobability <- rank(newgen$modelmeasure, ties.method="random")
+  newgen$selectionprobability <- 1/newgen$selectionprobability / sum(1/newgen$selectionprobability, na.rm=TRUE)
+  newgen$selectionprobability[is.na(newgen$selectionprobability)] <- 0
+
+  # update the generation counter
+  newgen$generation <- lastgennum + 1
+
+  # add this new generation to the population
+  models <- rbind(models, newgen)
+
+  # simplify rownames
+  rownames(models) <- 1:nrow(models)
+
+  return(models)
+
+}
