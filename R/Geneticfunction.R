4618d7d6016703061d659b91f84afe26e3960676
Ramcharan Reddy.K <ramcharankankanala@gmail.com>
2020-08-04 19:16:23 -0500

    Update Geneticfunction.R

--- R/Geneticfunction.R
+++ R/Geneticfunction.R
@@ -27,43 +27,42 @@ geneticimplement<- function(
   savenames <- adjacency$NewPCODE
   adjacency <- nb2listw(poly2nb(adjacency, queen=TRUE, row.names=adjacency$NewPCODE),
                         style="B")
-  print(adjacency)
+  #print(adjacency)
   adjacency2
   
   
 # set up the first generation of models
-modelsdf <- data.frame(generation=1,
+modelsdf1 <- data.frame(generation=1,
                        mutation="firstgen",
                        modelnumber=1:individpergeneration,
                        modelmeasure=-Inf,
                        selectionprobability=1/individpergeneration)
-print(modelsdf)
+#print(modelsdf)
 # set up initial clusters
-modelsdf$clustermat <- matrix(rep(NA,
-                                  size=nrow(modelsdf)*length(unique(mal$placeid)),
+modelsdf1$clustermat <- matrix(rep(NA,
+                                  size=nrow(modelsdf1)*length(unique(mal$placeid)),
                                   replace=TRUE),
-                              nrow=nrow(modelsdf),
+                              nrow=nrow(modelsdf1),
                               ncol=length(unique(mal$placeid)))
 
-modelsdf$clustermat[2,]
-dim(modelsdf$clustermat)[2]
-for (i in 1:nrow(modelsdf)) {
+modelsdf1$clustermat[2,]
+dim(modelsdf1$clustermat)[2]
+for (i in 1:nrow(modelsdf1)) {
   
-  modelsdf$clustermat[i, sample(x=dim(modelsdf$clustermat)[2],
+  modelsdf1$clustermat[i, sample(x=dim(modelsdf1$clustermat)[2],
                                 size=initialclusters,
                                 replace=FALSE)] <- 1:initialclusters
   
 }
-colnames(modelsdf$clustermat) <- unique(mal$placeid)
-modelsdf$clustermat
-unique(mal$placeid)
-modelsdf
+colnames(modelsdf1$clustermat) <- unique(mal$placeid)
+
+modelsdf1
 # create sample adjacency
 adjacency2
-adjacency2$cluster <- factor(fillbynearest(adjacency=adjacency, covariate=modelsdf$clustermat[1,]))
+adjacency2$cluster <- factor(fillbynearest(adjacency=adjacency, covariate=modelsdf1$clustermat[1,]))
 
 ggplot(adjacency2) + geom_sf(aes(fill=cluster))
-adjacency2$representative <- modelsdf$clustermat[1,]
+adjacency2$representative <- modelsdf1$clustermat[1,]
 adjacency2$representative
 ggplot(adjacency2) + geom_sf(aes(fill=cluster)) +
   theme(legend.position="none")
@@ -73,21 +72,23 @@ adjacency2$cluster <- NULL
 adjacency2$representative <- NULL
 
 # set up initial covariates
+envnames <- colnames(env)
+envnames <- envnames[!(envnames %in% c("placeid", "date", "year", "doy"))]
 envnames <- paste(envnames, "mat", sep="")
 
-print(envnames)
-modelsdf
+#print(envnames)
+modelsdf1
 
 
 
-numofcovnts<- 10
+
 
 
 x <- paste0("cov", seq(1:numofcovnts))
 
 
 for (j in x){
-  print(j)
+ # print(j)
   modelsdf1[j] <- sample(x=envnames,size=nrow(modelsdf1),
                          replace=TRUE)
 }
@@ -105,7 +106,7 @@ modelsdf1
 modelsdf1$modelmeasure <- Inf
 modelsdf1$selectionprobability <- 1
 modelsdf1$generation <- 1
-modelsdf
+
 modelsdf1
 
 
@@ -115,7 +116,9 @@ modelsdf1
 modelsdf1$modelmeasure <- evaluategenerationf(models=modelsdf1,
                                               modeldata=mal,
                                               adjacency=adjacency,
-                                              numberofcovnts = numofcovnts)
+                                              numofcovnts = numofcovnts)
+
+print("First generation evaluated")
 # rank models
 modelsdf1$modelmeasure
 modelsdf1$selectionprobability <- rank(modelsdf1$modelmeasure, ties.method="random")
@@ -135,23 +138,15 @@ for (generation in 2:generations) {
                               covariatenames=envnames,
                               individualspergeneration=individpergeneration,
                               adjacency=adjacency,
-                              numberofcovnts = numofcovnts)
+                              numofcovnts = numofcovnts)
   
-  
-  if (generation %% 2 == 0) {
-    
-    write.csv(modelsdf1,
-              paste(".\\outputs\\generation_",
-                    generation, ".csv",
-                    sep=""))
-    
-  }
   
 }
 
 
 # map best model
 bestmodel <- modelsdf1[modelsdf1$modelmeasure == min(modelsdf1$modelmeasure, na.rm=TRUE),]
+print("Best model is taken")
 bestmodel <- bestmodel[1,]
 bestmod <- data.frame(t(bestmodel$clustermat))
 bestmod$NewPCODE <- rownames(bestmod)
