870f84fe241a50d126c9346c8ea6d9cd8e947d7e
Ram <ramcharankankanala47@gmail.com>
2020-09-12 19:54:05 -0500

    added code with yes or no options

--- R/geneticimplement.R
+++ R/geneticimplement.R
@@ -1,10 +1,12 @@
 geneticimplement <- function(individpergeneration = NULL,
-                             initialclusters      = NULL,
-                             initialcovars        = NULL,
-                             generations          = NULL,
-                             modeldata            = NULL,
-                             envdata              = NULL,
-                             shapefile            = NULL) {
+                              initialclusters      = NULL,
+                              initialcovars        = NULL,
+                              generations          = NULL,
+                              modeldata            = NULL,
+                              envdata              = NULL,
+                              shapefile            = NULL,
+                              option               = NULL,
+                              Genmodelsdf          = NULL) {
 
   adjacency <- shapefile
 
@@ -18,100 +20,170 @@ geneticimplement <- function(individpergeneration = NULL,
                                 row.names=adjacency$placeid),
                         style="B")
 
-  # set up the first generation of models
-  modelsdf <- data.frame(generation=1,
-                         mutation="firstgen",
-                         modelnumber=1:individpergeneration,
-                         modelmeasure=Inf,
-                         selectionprobability=1/individpergeneration)
+  if (option == 'no'){
+    # set up the first generation of models
+    modelsdf <- data.frame(generation=1,
+                           mutation="firstgen",
+                           modelnumber=1:individpergeneration,
+                           modelmeasure=Inf,
+                           selectionprobability=1/individpergeneration)
 
-  # set up initial clusters and covariates
-  envnames <- colnames(env)
-  # this next line shouldn't be hardcoded
-  envnames <- envnames[!(envnames %in% c("placeid", "date", "year", "doy"))]
+    # set up initial clusters and covariates
+    envnames <- colnames(env)
+    # this next line shouldn't be hardcoded
+    envnames <- envnames[!(envnames %in% c("placeid", "date", "year", "doy"))]
 
-  modelsdf$clusterseeds <- ""
-  modelsdf$covars       <- ""
-  for (i in 1:nrow(modelsdf)) {
+    modelsdf$clusterseeds <- ""
+    modelsdf$covars       <- ""
+    for (i in 1:nrow(modelsdf)) {
 
-    modelsdf$clusterseeds[i] <- paste(sample(unique(modeldata$placeid),
-                                             size=initialclusters,
-                                             replace=FALSE),
-                                      1:initialclusters,
-                                      sep=",",
-                                      collapse=",")
+      modelsdf$clusterseeds[i] <- paste(sample(unique(modeldata$placeid),
+                                               size=initialclusters,
+                                               replace=FALSE),
+                                        1:initialclusters,
+                                        sep=",",
+                                        collapse=",")
 
-    modelsdf$covars[i] <- paste(sample(envnames,
-                                       size=initialcovars,
-                                       replace=FALSE),
-                                collapse=",")
+      modelsdf$covars[i] <- paste(sample(envnames,
+                                         size=initialcovars,
+                                         replace=FALSE),
+                                  collapse=",")
 
-  }
+    }
 
-  # set up initial model measures
-  modelsdf$modelmeasure <- Inf
-  modelsdf$selectionprobability <- 1
-  modelsdf$generation <- 1
+    # set up initial model measures
+    modelsdf$modelmeasure <- Inf
+    modelsdf$selectionprobability <- 1
+    modelsdf$generation <- 1
 
-  # evaluate the first generation
-  modelsdf$modelmeasure <- evaluategeneration(models=modelsdf,
-                                              modeldata=mal,
-                                              adjacency=adjacency,
-                                              placeids=placeids)
+    # evaluate the first generation
+    modelsdf$modelmeasure <- evaluategeneration(models=modelsdf,
+                                                modeldata=mal,
+                                                adjacency=adjacency,
+                                                placeids=placeids)
 
-  # rank models
-  modelsdf$selectionprobability <- rank(modelsdf$modelmeasure, ties.method="random")
-  modelsdf$selectionprobability <- 1/modelsdf$selectionprobability / sum(1/modelsdf$selectionprobability, na.rm=TRUE)
-  modelsdf$selectionprobability[is.na(modelsdf$selectionprobability)] <- 0
+    # rank models
+    modelsdf$selectionprobability <- rank(modelsdf$modelmeasure, ties.method="random")
+    modelsdf$selectionprobability <- 1/modelsdf$selectionprobability / sum(1/modelsdf$selectionprobability, na.rm=TRUE)
+    modelsdf$selectionprobability[is.na(modelsdf$selectionprobability)] <- 0
 
-  # save the first generation
-  write.csv(modelsdf, ".\\outputs\\generation_1.csv")
+    # save the first generation
+    write.csv(modelsdf, ".\\outputs\\generation_1.csv")
 
-  gens <- c()
-  measure <- c()
+    gens <- c()
+    measure <- c()
 
-  count = 0
+    count = 0
 
-  # run the rest of the generations
-  for (generation in 2:generations) {
+    # run the rest of the generations
+    for (generation in 2:generations) {
 
-    modelsdf <- addgeneration(models=modelsdf,
-                              modeldata=mal,
-                              covariatenames=envnames,
-                              individualspergeneration=individpergeneration,
-                              adjacency=adjacency,
-                              placeids=placeids)
+      modelsdf <- addgeneration(models=modelsdf,
+                                modeldata=mal,
+                                covariatenames=envnames,
+                                individualspergeneration=individpergeneration,
+                                adjacency=adjacency,
+                                placeids=placeids)
 
-    #if (generation == generations) {
+      #if (generation == generations) {
 
       write.csv(modelsdf,
                 paste(".\\outputs\\generation_",
                       generation, ".csv",
                       sep=""))
 
-    #}
+      #}
 
 
       gens <- c(gens, generation)
       measure <- c(measure,min(modelsdf$modelmeasure))
 
 
+    }
+
+    for (i in 1:length(gens)){
+
+      count = count + 1
+      if (count == 10){
+
+        plot(head(gens,10),head(measure,10),type="l", main="generations vs measure",xlab = "Generations",
+             ylab="Model min measure")
+        # print(gens[1:2])
+        # print(measure[1:2])
+        gens <- gens[-seq(1:10)]
+        measure <- measure[-seq(1:10)]
+
+        count = 0
+      }
+    }
+
   }
 
-  for (i in 1:length(gens)){
+  if (option == 'yes') {
+    # set up initial clusters and covariates
+    envnames <- colnames(env)
+    # this next line shouldn't be hardcoded
+    envnames <- envnames[!(envnames %in% c("placeid", "date", "year", "doy"))]
+    modelsdf <- Genmodelsdf
 
-    count = count + 1
-    if (count == 10){
+    print("You selected to run genetic algoithm from some generations")
 
-      plot(head(gens,10),head(measure,10),type="l", main="generations vs measure",xlab = "Generations",
-           ylab="Model min measure")
-     # print(gens[1:2])
-     # print(measure[1:2])
-      gens <- gens[-seq(1:10)]
-      measure <- measure[-seq(1:10)]
+    print(head(modelsdf))
 
-      count = 0
+    # save the first generation
+    write.csv(modelsdf, paste(".\\outputs\\generation_",
+                              tail(Genmodelsdf,1)$generation, ".csv",
+                              sep=""))
+
+    gens <- c()
+    measure <- c()
+
+    count = 0
+
+    # run the rest of the generations
+    for (generation in tail(Genmodelsdf,1)$generation + 1 :generations) {
+
+      print(tail(Genmodelsdf,1)$generation + 1)
+
+      modelsdf <- addgeneration(models=modelsdf,
+                                modeldata=mal,
+                                covariatenames=envnames,
+                                individualspergeneration=individpergeneration,
+                                adjacency=adjacency,
+                                placeids=placeids)
+
+      #if (generation == generations) {
+
+      write.csv(modelsdf,
+                paste(".\\outputs\\generation_",
+                      generation, ".csv",
+                      sep=""))
+
+      #}
+
+
+      gens <- c(gens, generation)
+      measure <- c(measure,min(modelsdf$modelmeasure))
+
+
     }
+
+    for (i in 1:length(gens)){
+
+      count = count + 1
+      if (count == 2){
+
+        plot(head(gens,2),head(measure,2),type="l", main="generations vs measure",xlab = "Generations",
+             ylab="Model min measure")
+        # print(gens[1:2])
+        # print(measure[1:2])
+        gens <- gens[-seq(1:2)]
+        measure <- measure[-seq(1:2)]
+
+        count = 0
+      }
+    }
+
   }
 
   return(modelsdf)
